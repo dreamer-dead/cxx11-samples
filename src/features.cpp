7e7c5df59f1a7e5b661845cf1dc382ca0c27f983
Andrey Kraynov <iceman@yandex-team.ru>
2013-06-28 18:55:16 +0400

    More variadic templates

--- src/features.cpp
+++ src/features.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <type_traits>
 
 // Common part
@@ -16,11 +17,31 @@ template <typename ... TypesList>
 struct varidic_inheritance : public TypesList ... {
 };
 
+// Static assert
 static_assert(
   sizeof(varidic_inheritance<int_holder, float_array_holder>)
     >= sizeof(int_holder) + sizeof(float_array_holder),
   "Invalid size!");
 
+// Recursion tail
+void out() { std::cout << std::endl; }
+
+template <typename Head, typename ... TailList>
+void out(const Head& head, TailList ... tail) {
+  std::cout << head << ' ';
+  out(tail...);
+}
+
+template <typename ... TypesList>
+void process_args(TypesList ... args) {
+  out(args...);
+}
+
 int main() {
+  std::string str("hello");
+  int i = 10;
+  double d = 0.65541;
+  process_args(str, i, d);
+
   return 0;
 }
